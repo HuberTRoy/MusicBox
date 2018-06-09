f9600180eb3dd60b110a48794cc2aa9064bb1813
weiy <b754048538@163.com>
2018-06-09 23:08:08 +0800

    update song source

--- MusicPlayer/apis/qqApi.py
+++ MusicPlayer/apis/qqApi.py
@@ -33,7 +33,8 @@ class QQApi(HttpRequest):
         # 随便写一个就可以，原本是根据cookies里一个根据时间变化的参数确定的。
         self.guid = 3768717388
         if not self.sip:
-            logger.info("获取QQToken失败。当前key: {0}, 当前sip: {1}".format(self.key, self.sip))
+            logger.info("获取QQToken失败。当前key: {0}, 当前sip: {1}".format(
+                self.key, self.sip))
             print('QQ 播放地址获取失败，请勿播放QQ音乐。')
 
     def httpRequest(self, *args, **kwargs):
@@ -43,58 +44,122 @@ class QQApi(HttpRequest):
         with ignored():
             return html.text
 
-        logger.info("url: {0} 请求失败. Header: {1}".format(args[0], kwargs.get('headers')))
+        logger.info("url: {0} 请求失败. Header: {1}".format(
+            args[0], kwargs.get('headers')))
         return ''
 
-    # copy from listen1.
-    # https://github.com/listen1/listen1/blob/master/listen1/replay/qq.py
     def _get_qqtoken(self):
-        token_url = 'http://base.music.qq.com/fcgi-bin/fcg_musicexpress.fcg?' + \
-            'json=3&guid=3768717388&g_tk=938407465&loginUin=0&hostUin=0&' + \
-            'format=jsonp&inCharset=GB2312&outCharset=GB2312&notice=0&' + \
-            'platform=yqq&jsonpCallback=jsonCallback&needNewCode=0'
-        
-        data = self.httpRequest(token_url, method='GET', headers=self.tokenHeaders)
-        
-        with ignored():
-            data = data[len("jsonCallback("):-len(");")]
+        """
+            更新后的不需要再获取token, sip变为固定URL:
+            http://dl.stream.qqmusic.qq.com/
+        """
+        # token_url = 'http://base.music.qq.com/fcgi-bin/fcg_musicexpress.fcg?' + \
+        #     'json=3&guid=3768717388&g_tk=938407465&loginUin=0&hostUin=0&' + \
+        #     'format=jsonp&inCharset=GB2312&outCharset=GB2312&notice=0&' + \
+        #     'platform=yqq&jsonpCallback=jsonCallback&needNewCode=0'
 
-            return json.loads(data)
-        return {'key': '1', 'sip': ['']}
+        # data = self.httpRequest(token_url, method='GET',
+        #                         headers=self.tokenHeaders)
+
+        # with ignored():
+        #     data = data[len("jsonCallback("):-2]
+
+        #     return json.loads(data)
+        return {'key': '1', 'sip': ['http://dl.stream.qqmusic.qq.com/']}
 
     def _getImgUrl(self, mid):
         imgUrl = 'https://y.gtimg.cn/music/photo_new/'
         return imgUrl + 'T002R300x300M000' + mid + '.jpg'
 
     def _getSongUrl(self, mid):
-        return '{0}C400{1}.m4a?vkey={2}&guid={3}'.format(self.sip, mid, self.key, self.guid)
+        vkey = self._getSongUrlVkey(mid)
+
+        if not vkey:
+            vkey = '000'
+
+        return '{0}C400{1}.m4a?vkey={2}&guid={3}&uin=0&fromtag=66'.format(self.sip, mid, vkey, self.guid)
+
+    def _getSongUrlVkey(self, mid):
+        # 获取得到QQ音乐歌曲地址所需的vkey。
+        # 返回的是vkey。
+
+        vkey_url = 'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg'
+        params = {
+            'g_tk': '5381',
+            'jsonpCallback': 'MusicJsonCallback8571665793949388',
+            'loginUin': '0',
+            'hostUin': '0',
+            'format': 'json',
+            'inCharset': 'utf8',
+            'outCharset': 'utf-8',
+            'notice': '0',
+            'platform': 'yqq',
+            'needNewCode': '0',
+            'cid': '205361747',
+            'callback': 'MusicJsonCallback8571665793949388',
+            'uin': '0',
+            'songmid': mid,
+            'filename': 'C400' + mid + '.m4a',
+            'guid': '{}'.format(self.guid)
+        }
+
+        response = self.httpRequest(vkey_url, method="GET", headers=self.headers, params=params)
+
+        with ignored():
+            data = json.loads(response[response.find("{"):-1])
+
+            return data['data']['items'][0]['vkey']
+
+        return False
+
+    def _fromSongUrlGetSongMid(self, songUrl):
+        # 根据一个完整的歌曲Url,获取出它的mid。
+        # 'http://dl.stream.qqmusic.qq.com/
+        # C400 0000ASDASD.m4a
+        # ?vkey=' + vkey + '&guid=7133372870&uin=0&fromtag=66'
+
+        songUrl = songUrl.split("?")[0]
+
+        return songUrl[songUrl.find('C400')+4:-4]
+
+    def getSongUrl(self, songUrl):
+        # songUrl格式：
+        #  1. 歌曲mid: 000xkbLI2QEKE9 这样的形式。
+        #  2. 完整URL，上次授权后得到的URL，但已过期需要重新获取。
+
+        mid = songUrl
+        if 'http' in songUrl:
+            mid = self._fromSongUrlGetSongMid(songUrl)
+
+        return self._getSongUrl(mid)
 
     def playList(self, ein=29):
         """
         ein控制返回的歌单。
         29, 59, 89....
         """
-        url = 'https://c.y.qq.com/splcloud/fcgi-bin/'+\
-        'fcg_get_diss_by_tag.fcg?rnd=0.5136307078685405&g_tk=5381&'+\
-        'jsonpCallback=getPlaylist&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8'+\
-        '&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&categoryId=10000000&'+\
-        'sortId=5&sin=30&ein={0}'.format(ein)
+        url = 'https://c.y.qq.com/splcloud/fcgi-bin/' +\
+            'fcg_get_diss_by_tag.fcg?rnd=0.5136307078685405&g_tk=5381&' +\
+            'jsonpCallback=getPlaylist&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8' +\
+            '&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&categoryId=10000000&' +\
+            'sortId=5&sin=30&ein={0}'.format(ein)
 
         response = self.httpRequest(url, method='GET', headers=self.headers)
 
         with ignored():
-            data = json.loads(response[len('getPlaylist('):-len(')')])
-        
+            data = json.loads(response[len('getPlaylist('):-1])
+
             return data['data']['list']
-        
+
         return False
 
     def getPlaylist(self, ids):
-        url = 'https://shc.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg?type=1&json=1&utf8=1&onlysong=0'+\
-        '&disstid={0}&format=jsonp&g_tk=5381&jsonpCallback=playlistinfoCallback&loginUin=0&hostUin=0&'.format(ids)+\
-        'format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0'
-        
-        response = self.httpRequest(url, method='GET', headers=self.playlistHeaders)
+        url = 'https://shc.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg?type=1&json=1&utf8=1&onlysong=0' +\
+            '&disstid={0}&format=jsonp&g_tk=5381&jsonpCallback=playlistinfoCallback&loginUin=0&hostUin=0&'.format(ids) +\
+            'format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0'
+
+        response = self.httpRequest(
+            url, method='GET', headers=self.playlistHeaders)
         with ignored():
             data = json.loads(response[len('playlistinfoCallback('):-len(')')])
             data = data['cdlist'][0]
@@ -110,7 +175,8 @@ class QQApi(HttpRequest):
 
             for i in songs:
                 i['name'] = i['songname']
-                i['artists'] = [{'name': ';'.join([x['name'] for x in i['singer']])}]
+                i['artists'] = [
+                    {'name': ';'.join([x['name'] for x in i['singer']])}]
                 i['duration'] = int(i['interval']) * 1000
                 # i['album'] = {'blurPicUrl': imgUrl + 'T002R300x300M000' + i['albummid'] + '.jpg'}
                 i['album'] = {'blurPicUrl': self._getImgUrl(i['albummid'])}
@@ -125,63 +191,43 @@ class QQApi(HttpRequest):
         return False
 
     def search(self, key):
-        url = 'https://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&'+\
-                   'new_json=1&remoteplace=txt.yqq.center&searchid=43541888870417375&t=0&aggr=1'+\
-                   '&cr=1&catZhida=1&lossless=0&flag_qc=0&p=1&n=50&'+\
-                   'w={0}'.format(urllib.parse.quote(key))+\
-                   '&g_tk=5381&jsonpCallback=searchCallbacksong6064&loginUin=0&hostUin=0&'+\
-                   'format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0'
+        url = 'https://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&' +\
+            'new_json=1&remoteplace=txt.yqq.center&searchid=43541888870417375&t=0&aggr=1' +\
+            '&cr=1&catZhida=1&lossless=0&flag_qc=0&p=1&n=50&' +\
+            'w={0}'.format(urllib.parse.quote(key)) +\
+            '&g_tk=5381&jsonpCallback=searchCallbacksong6064&loginUin=0&hostUin=0&' +\
+            'format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0'
 
         response = self.httpRequest(url, method='GET')
 
         with ignored():
-            data = json.loads(response[len('searchCallbacksong6064('):-len(')')])
-            
+            data = json.loads(
+                response[len('searchCallbacksong6064('):-1])
+
             data = data['data']['song']
 
             newDatas = {}
             newDatas['songCount'] = data['curnum'] - 1
             songs = []
             for i in data['list']:
-                songs.append({ 'name': i['name'],
-                                                  'ar': [{'name': ';'.join([x['name'] for x in i['singer']])}],
-                                                  'al': {'picUrl': self._getImgUrl(i['album']['mid'])},
-                                                  'dt': i['interval'] * 1000,
-                                                  'id': i['id'],
-                                                  # 当然这里不是mp3，为了统一接口这样写。
-                                                  'mp3Url': self._getSongUrl(i['mid']),
-                                                  'lyric': 'qq'
-                    })
-
+                songs.append({'name': i['name'],
+                              'ar': [{'name': ';'.join([x['name'] for x in i['singer']])}],
+                              'al': {'picUrl': self._getImgUrl(i['album']['mid'])},
+                              'dt': i['interval'] * 1000,
+                              'id': i['id'],
+                              # 当然这里不是mp3，为了统一接口这样写。
+                              'mp3Url': i['mid'],
+                              'lyric': 'qq'
+                              })
             newDatas['songs'] = songs
-            
-            return newDatas 
+
+            return newDatas
 
         return False
+
 
 qqApi = QQApi()
 
 
 if __name__ == '__main__':
-    pass
-    # a = qqApi.playList(29)
-    # http://dl.stream.qqmusic.qq.com/
-    # C400 0000HZ4N01bJ8a.m4a
-    #           0000HZ4N01bJ8a
-    # ?vkey=42BBA28D626228E8C360FEFE03817E3A15885426F969484963ABFBC4D197A5CEF4E3ED7911F3236150212564FA9F2F1B0AD02AF37FF181A2&guid=3768717388&uin=0&fromtag=66
-    # ?vkey=1B6C73D1685155EBB75D36995A81FD2DEFEA2BA30389B99675EDDB367BF4FFCFC075B63FBFFA9F4642A411C02A248FEF24E91379DBDEBA7D&guid=780782017&uin=0&fromtag=66
-    # a = qqApi._get_qqtoken()
-    # print(qqApi.key, qqApi.sip)    
-    a = qqApi.search("故梦")
-    print(a)
-    for i in a:
-        print(i)
-    #     print(a[i])
-    # print(len(a['list']))
-        # print(i['d
-        # issname'])
-        # print(i['dissid'])
-
-    # for i in a:
-        # print(i)
-    #     print(a[i])
+    help(qqApi)
